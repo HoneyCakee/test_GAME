3b414bdb9ddc580a040e7777a5dd13a8e4691526
HoneyCakee <38957908+HoneyCakee@users.noreply.github.com>
2018-05-04 20:05:12 +0300

    UI Hunger&Thirst

---
+++ GAME/Assets/Scripts/PlayerHung.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+public class PlayerHung : MonoBehaviour {
+
+	public float currentHungry = 100;
+	public float time = 1f;
+
+	public Sprite full;
+	public Sprite H3;
+	public Sprite H2;
+	public Sprite H1;
+	public Sprite empty;
+
+
+	private float minHungry = 0;
+	private float maxHungry = 100;
+
+	Image Image;
+
+
+	void Start () {
+		Image = GetComponent<Image> ();
+		StartCoroutine (hungry (5));
+	}
+
+
+	void Update () {
+
+		}
+
+	IEnumerator hungry (float h){
+		while (currentHungry >= minHungry) {
+
+			yield return new WaitForSeconds (time);
+			currentHungry -= h;
+			if (currentHungry == 70) {
+				Image.sprite = H3;
+			}
+
+			if (currentHungry == 50) {
+				Image.sprite = H2;
+			}
+
+			if (currentHungry == 25) {
+				Image.sprite = H1;
+			}
+
+			if (currentHungry == 0) {
+				Image.sprite = empty;
+			}
+			}
+		}
+}
---
+++ GAME/Assets/Scripts/PlayerThirst.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+public class PlayerThirst : MonoBehaviour {
+
+	public float currentThirst = 100;
+	public float time = 1f;
+
+	public Sprite full;
+	public Sprite H3;
+	public Sprite H2;
+	public Sprite H1;
+	public Sprite empty;
+
+
+	private float minThirst = 0;
+	private float maxThirst = 100;
+
+	Image Image;
+
+
+	void Start () {
+		Image = GetComponent<Image> ();
+		StartCoroutine (thirst (5));
+	}
+
+
+	void Update () {
+
+	}
+
+	IEnumerator thirst (float h){
+		while (currentThirst >= minThirst) {
+
+			yield return new WaitForSeconds (time);
+			currentThirst -= h;
+			if (currentThirst == 70) {
+				Image.sprite = H3;
+			}
+
+			if (currentThirst == 50) {
+				Image.sprite = H2;
+			}
+
+			if (currentThirst == 25) {
+				Image.sprite = H1;
+			}
+
+			if (currentThirst == 0) {
+				Image.sprite = empty;
+			}
+		}
+	}
+}
